8213316febbd7c2e991dccfc8bbe3c12af3bdaf7
Hp <architgupta69@yahoo.in>
2016-07-25 20:24:34 +0530

    ids defined

--- app/src/main/java/com/vizy/newsapp/realread/activities/SignIn.java
+++ app/src/main/java/com/vizy/newsapp/realread/activities/SignIn.java
@@ -1,15 +1,35 @@
 package com.vizy.newsapp.realread.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.vizy.newsapp.realread.R;
 
 public class SignIn extends AppCompatActivity {
 
+    Button signIn, noAccount;
+    EditText mobileNo, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        signIn=(Button)findViewById(R.id.sign_in);
+        noAccount=(Button)findViewById(R.id.no_account);
+        mobileNo=(EditText)findViewById(R.id.enter_mobile_no_login);
+        password=(EditText)findViewById(R.id.enter_password_login);
+
+        noAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toSignUp=new Intent(SignIn.this, SignUp.class);
+                startActivity(toSignUp);
+            }
+        });
     }
 }
--- app/src/main/java/com/vizy/newsapp/realread/activities/SignUp.java
+++ app/src/main/java/com/vizy/newsapp/realread/activities/SignUp.java
@@ -2,14 +2,23 @@ package com.vizy.newsapp.realread.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.vizy.newsapp.realread.R;
 
 public class SignUp extends AppCompatActivity {
 
+    EditText mobile, password, confirm_password;
+    Button done_signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mobile=(EditText)findViewById(R.id.enter_mobile_no);
+        password=(EditText)findViewById(R.id.enter_password);
+        confirm_password=(EditText)findViewById(R.id.enter_password_again);
     }
 }
