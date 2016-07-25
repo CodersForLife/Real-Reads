061e3ca15b819aa14bfdd49955261bce7b1078e2
Hp <architgupta69@yahoo.in>
2016-07-25 17:07:47 +0530

    sign in and sign up layouts added

---
+++ app/src/main/java/com/vizy/newsapp/realread/activities/SignUp.java
@@ -0,0 +1,15 @@
+package com.vizy.newsapp.realread.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vizy.newsapp.realread.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
