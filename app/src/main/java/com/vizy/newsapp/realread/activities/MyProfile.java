1c9d45d879601fbdca59aa65598c5f6aed7cb055
dell <piyush6348@gmail.com>
2016-08-05 01:57:20 +0530

    nothing added

--- app/src/main/java/com/vizy/newsapp/realread/activities/MyProfile.java
+++ app/src/main/java/com/vizy/newsapp/realread/activities/MyProfile.java
@@ -2,6 +2,7 @@ package com.vizy.newsapp.realread.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.vizy.newsapp.realread.R;
 import com.vizy.newsapp.realread.model.UserSession;
@@ -10,10 +11,15 @@ import java.util.HashMap;
 
 public class MyProfile extends AppCompatActivity {
 
+    private EditText profileName,profileEmail,profilePhone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+        profileName=(EditText) findViewById(R.id.profile_name);
+        profileEmail=(EditText) findViewById(R.id.profile_email);
+        profilePhone=(EditText) findViewById(R.id.profile_number);
 
         UserSession session = new UserSession(getApplicationContext());
 
