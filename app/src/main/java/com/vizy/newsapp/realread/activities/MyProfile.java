c4d3e460f1d4fb44b0d1cbfd84bc7f82e6ca4ed5
dell <piyush6348@gmail.com>
2016-08-05 02:06:18 +0530

    profile

--- app/src/main/java/com/vizy/newsapp/realread/activities/MyProfile.java
+++ app/src/main/java/com/vizy/newsapp/realread/activities/MyProfile.java
@@ -2,7 +2,9 @@ package com.vizy.newsapp.realread.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.vizy.newsapp.realread.R;
 import com.vizy.newsapp.realread.model.UserSession;
@@ -12,6 +14,7 @@ import java.util.HashMap;
 public class MyProfile extends AppCompatActivity {
 
     private EditText profileName,profileEmail,profilePhone;
+    private TextView profileNameTextview,profileEmailTextview,profileNumberTextview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +24,26 @@ public class MyProfile extends AppCompatActivity {
         profileEmail=(EditText) findViewById(R.id.profile_email);
         profilePhone=(EditText) findViewById(R.id.profile_number);
 
+        profileEmailTextview=(TextView)findViewById(R.id.profile_email_textview);
+        profileNameTextview=(TextView)findViewById(R.id.profile_name_textview);
+        profileNumberTextview=(TextView)findViewById(R.id.profile_phone_textview);
+
         UserSession session = new UserSession(getApplicationContext());
 
         HashMap<String, String> user = session.getUserDetails();
         String name = user.get(UserSession.KEY_NAME);
         String email = user.get(UserSession.KEY_EMAIL);
+
+        if(name.length()>0)
+        {
+            profileNameTextview.setVisibility(View.VISIBLE);
+            profileName.setVisibility(View.GONE);
+            profileNameTextview.setText(name);
+        }
+        if(email.length()>0){
+            profileEmailTextview.setVisibility(View.VISIBLE);
+            profileEmail.setVisibility(View.GONE);
+            profileEmailTextview.setText(email);
+        }
     }
 }
